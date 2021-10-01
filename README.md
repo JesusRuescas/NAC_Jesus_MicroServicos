556c934593a6b24c5f3338ce9edf0cdc82664d1e
Jesus Ruescas Junior <jesus.junior.1999@hotmail.com>
2021-10-01 18:13:30 -0300

    Initial commit

---
+++ README.md
@@ -0,0 +1,2 @@
+# NAC_Jesus_MicroServicos
+ 3SIS - Nac1 | RM 83146 
---
+++ src/app.js
@@ -0,0 +1,28 @@
+const express = require("express");
+const mongoose = require("mongoose");
+const app = express();
+app.use(express.json());
+app.use(express.urlencoded({ extended: true }));
+
+mongoose.connect();
+
+app.use(function (req, res, next) {
+  res.header("Access-Control-Allow-Origin", "*");
+  res.header(
+    "Access-Control-Allow-Headers",
+    "Origin, X-Requested-With, Content-Type, Accept, x-access-token"
+  );
+  res.header("Access-Control-Allow-Methods", "GET, POST, PUT, DELETE");
+  next();
+});
+
+//Registar as models
+require("./models/user");
+
+//Rotas
+const userRouter = require("./routes/user-route");
+
+app.use("/users", userRouter);
+
+//meu erro
+module.exports = app;
